c06f0f24f97e34d0dab49c5cddb5cef9c6fe648f
Assaf Kaner <kaner.assaf@gmail.com>
2021-02-17 21:45:21 +0200

    PUT bin entry point

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-
+app.use(express.json());
 
 app.get('/v3/b/:id', (req, res) => {
-    const id = req.params.id;
+    const {id} = req.params;
     try{
         const binContent = fs.readFileSync(`./bins/${id}.json`);
         res.send(binContent);
@@ -12,6 +12,32 @@ app.get('/v3/b/:id', (req, res) => {
         res.status(422).json({"message":"Invalid Record ID"});
     }
 });
+
+app.put('/v3/b/:id', (req, res) => {
+    const body = req.body;
+    console.log("body:", body);
+    const {id} = req.params;
+    const binExist = fs.existsSync(`./bins/${id}.json`);
+    if(!binExist){
+        res.status(404).json({
+            "message": "Bin not found",
+            "success": false
+          });
+        return;
+    }
+    fs.writeFileSync(`./bins/${id}.json`, JSON.stringify(body,null,4));
+    const successMessage = {
+        success: true,
+        data: body,
+        "version": 1,
+        "parentId": id
+    }
+    res.send(successMessage);
+});
+
+// app.post('/v3/b/:id', (req, res) => {
+
+// })
 
 app.listen(3000, () => {
     console.log("app is running on port 3000");
