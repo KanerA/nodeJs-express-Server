2548f1d177d676b8fcf6ecbf34bce470787ff581
Assaf Kaner <kaner.assaf@gmail.com>
2021-02-20 17:33:42 +0200

    remove error handling from main code
    most of error handling is done by the middlewares in utils.js
    line 51, inserts the uuidv4 generated ID to the body of the request object.

--- index.js
+++ index.js
@@ -1,53 +1,36 @@
 const express = require('express');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
+const { checkId } = require('./utils');
+const { checkBin } = require('./utils');
+const { checkIfBlank } = require('./utils');
 const app = express();
 const dir = './bins';
 app.use(express.json());
 
-const bins =[];
 
 app.get('/v3/b', (req, res) => {
   const binsContent = [];
-  try{
     fs.readdir(dir, (err, files) => { 
-      if(!files){
-        res.status(200).send('No files to show');
-      }
-      for(file of files){
-        const fileContent = JSON.parse(fs.readFileSync(`./bins/${file}`));
-        let temp = fileContent.body;
-        binsContent.push(temp);
-      }
-      res.send(JSON.stringify(binsContent));
+        for(file of files){
+          const fileContent = JSON.parse(fs.readFileSync(`./bins/${file}`));
+          let temp = fileContent;
+          binsContent.push(temp);
+        }
+        res.status(200).send(JSON.stringify(binsContent));
     })
-  } catch(err) {
-    console.log(err + "Error");
-  }
 });
 
-app.get('/v3/b/:id', (req, res) => {
+app.get('/v3/b/:id', checkId, checkBin, (req, res) => {
     const {id} = req.params;
-    try{
-        const binContent = fs.readFileSync(`./bins/${id}.json`);
-        res.send(binContent);
-    } catch(e) {
-        res.status(422).json({"message":"Invalid Record ID"});
-    }
+      const binContent = fs.readFileSync(`./bins/${id}.json`);
+      res.send(binContent);
 });
 
-app.put('/v3/b/:id', (req, res) => {
-    const body = req.body;
+app.put('/v3/b/:id', checkId, checkBin, checkIfBlank, (req, res) => {
+    const {body} = req;
     const {id} = req.params;
-    const binExist = fs.existsSync(`./bins/${id}.json`);
-    if(!binExist){
-        res.status(404).json({
-            "message": "Bin not found",
-            "success": false
-          });
-        return;
-    }
-    fs.writeFileSync(`./bins/${id}.json`, JSON.stringify(({body , "id": id}),null,4));
+    fs.writeFileSync(`./bins/${id}.json`, JSON.stringify(body, null, 4));
     const successMessage = {
         success: true,
         data: body,
@@ -57,33 +40,27 @@ app.put('/v3/b/:id', (req, res) => {
     res.send(successMessage);
 });
 
-app.post('/v3/b', (req, res) => {
-    const {body} = req;
+app.post('/v3/b', checkIfBlank, (req, res) => {
+  const {body} = req;
     const binId = uuidv4();
     const binsDirExist = fs.existsSync(`./bins`);
     if(!binsDirExist){ // if the bins directory does not exist, it creates one
       fs.mkdirSync('./bins');
-    }
-  try {
-    fs.writeFileSync(
-    `./bins/${binId}.json`,
-    JSON.stringify( {body , 'id': binId}, null, 4)
-    );
-    res.status(200).send(JSON.stringify({"success":"true", "data":body , "id":binId}));
-  } catch (e) {
-      res.status(500).json(JSON.stringify({ message: "Error!", error: e }));
     } 
+    body.id = binId;
+    fs.writeFileSync(
+    `./bins/${binId}.json`, JSON.stringify( body, null, 4));
+    res.status(200).json({"success":"true", "data":body , "id":binId}); 
 });
 
-app.delete('/v3/b/:id', (req, res) => {
+app.delete('/v3/b/:id', checkId, (req, res) => {
   const {id} = req.params;
-  console.log('hello');
   try{
     fs.unlinkSync(`./bins/${id}.json`);
     console.log('file deleted successfully');
-    res.json(JSON.stringify({"success": true, "id": id, "message": "Bin "+ id +" is deleted successfully."}));
-  } catch{
-    res.status(404).json({"message": "Bin not found","success": false})
+    res.json({"success": true, "id": id, "message": "Bin "+ id +" was deleted successfully."});
+  } catch(err){
+    res.status(401).json({"message": "Bin not found or it doesn't belong to your account", err})
   };
 });
 
