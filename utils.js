9556c188311e0459e1141372e7f77da93ea7a4ee
Assaf Kaner <kaner.assaf@gmail.com>
2021-02-20 17:23:43 +0200

    create checkBin middleware
    this middleware checks if the bin specified in the request, exists.
    if not , it throws a 404 error.

--- utils.js
+++ utils.js
@@ -6,6 +6,15 @@ const checkId = (req, res, next) => {
     if(id.length !== 36){
         res.status(400).send('{"message": "Invalid Bin Id provided"}');
     }
-  
     next();
-  };
+};
+
+const checkBin = (req, res, next) => {
+    const { id } = req.params;
+    const binExist = fs.existsSync(`./bins/${id}.json`);
+    if(!binExist){
+        res.status(404).json({"message": "Bin Not Found"});
+        return;
+    }
+    next();
+};
