1837749687e4258167fb27c7756b582ed745a491
Assaf Kaner <kaner.assaf@gmail.com>
2021-02-24 19:32:09 +0200

    add GET testing

--- index.test.js
+++ index.test.js
@@ -32,3 +32,23 @@ beforeAll(() => {
         JSON.stringify(expectedRes)
     );
 });
+
+describe("Test GET functionality", () => {
+
+    it("Should get a bin by id", async () => {
+        const res = await request(app).get(`/v3/b/${expectedRes.id}`);
+        expect(res.status).toBe(200);
+        expect(res.body).toEqual(expectedRes);
+    });
+
+    test("if an illegal id is requested an appropriate response is sent", async () => {
+        const res = await request(app).get(`/v3/b/${expectedRes.id}1`);
+        expect(res.status).toBe(400);
+        expect(res.text).toBe('{"message":"Invalid Bin Id provided"}')
+    });
+    test("if a bin is not found an appropriate response is sent", async () => {
+        const res = await request(app).get(`/v3/b/a24eb7e7-9f4e-464d-9e55-b78d8ff5753c`);
+        expect(res.status).toBe(404);
+        expect(res.text).toBe('{"message":"Bin Not Found"}');
+    });
+});
